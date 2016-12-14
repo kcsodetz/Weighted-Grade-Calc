19b398a80f0d0443dd07d5a78c06767e8443551f
Ken Sodetz <kensodetz@gmail.com>
2016-12-14 17:44:47 -0500

    Added the calculation methods in driver

--- src/CalculateGUI.java
+++ src/CalculateGUI.java
@@ -6,11 +6,9 @@ import javax.swing.*;
 /**
  * Created by Ken Sodetz on 12/14/2016.
  */
-public class CalculateGUI extends Frame implements ActionListener {
+public class CalculateGUI extends Frame implements ActionListener { //TODO implement GUI
 
-    //container for the content
-    private Container coontentPane;
-
+    private Container getContentPane;
     //buttons
     private JButton calculate;
     private JTextField input;
@@ -19,16 +17,48 @@ public class CalculateGUI extends Frame implements ActionListener {
 
     public CalculateGUI(){
         this.setTitle("Weighted Grade Calculator");
-
+        //this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         addElements();
     }
 
-    private void addElements(){
+    private void addElements(){ //TODO finish method
+
+        JPanel contentPanel, buttonPanel;
+
+        Container contentPane = this.getContentPane;
+        contentPane.setLayout(new BorderLayout());
+
+        contentPanel = new JPanel(new BorderLayout());
+        contentPanel.setBorder(BorderFactory.createTitledBorder("Input"));
+
+        buttonPanel = new JPanel(new BorderLayout());
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder());
+
+        contentPanel.add(buttonPanel, BorderLayout.NORTH);
+
+        calculate = new JButton("Calculate");
+        calculate.addActionListener(this);
+
+        contentPane.add(contentPanel, BorderLayout.NORTH);
+        contentPane.add(buttonPanel, BorderLayout.CENTER);
+        contentPane.add(calculate, BorderLayout.SOUTH);
+
+    }
+
+    public void createContentPanel(){ //TODO finish method
 
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) { //TODO finish action listener
+        String str;
 
+
+    }
+
+    public static void main(String[] args) { //TODO finish main
+        CalculateGUI window = new CalculateGUI();
+        window.pack();
+        window.setVisible(true);
     }
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,64 @@
-public class Driver{
-    public static void main(String[] args) {
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+public class Driver{
+
+    private HashMap <String, Double> gradesMap;
+    private Scanner in = new Scanner(System.in);
+    private double[] numArray;
+
+    public Driver(){
+        gradesMap = new HashMap<>();
+        numArray= new double[2];
+    }
+
+    private void getGrades(int line){
+
+        Boolean isDouble;
+        double tempGrade = 0;
+        for (int i = 0; i < line; i++) {
+            System.out.print("Enter the name of the Category " +(i+1)+ ": ");
+            in.nextLine();
+            String temp = in.nextLine();
+            do {
+                try {
+                    isDouble = true;
+                    System.out.print("Enter the percent of category " + temp + " (E.g: 94.5): ");
+                    tempGrade = in.nextDouble();
+                } catch (InputMismatchException e) {
+                    System.out.println("Percentage must be a Number");
+                    isDouble = false;
+                    in.next();
+                }
+            } while (!isDouble);
+            tempGrade = tempGrade / 100;
+            gradesMap.put(temp, tempGrade);
+        }
+    }
+
+    private int getNumberOfCategories(){
+        int temp = 0;
+        Boolean isInt;
+        do {
+            isInt = true;
+            try {
+                System.out.print("Enter the number of weighted categories: ");
+                temp = in.nextInt();
+            } catch (InputMismatchException e){
+                isInt = false;
+                System.out.println("Number of categories must be an integer");
+                in.next();
+            }
+        } while (!isInt);
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+        driver.getGrades(driver.getNumberOfCategories());
     }
 }
