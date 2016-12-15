c8496f13cc3671a2b3771ce55f1acabee3c5df61
Ken Sodetz <kensodetz@gmail.com>
2016-12-14 19:51:13 -0500

    added inputWeight and custom exception

--- src/Driver.java
+++ src/Driver.java
@@ -1,27 +1,24 @@
-import com.sun.org.apache.xpath.internal.operations.Bool;
-
-import java.util.HashMap;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class Driver{
 
-    private HashMap <String, Double> gradesMap;
+    private HashMap <String, double[]> gradesMap;
     private Scanner in = new Scanner(System.in);
-    private double[] numArray;
+
 
     public Driver(){
         gradesMap = new HashMap<>();
-        numArray= new double[2];
     }
 
-    private void getGrades(int line){
+    private void inputGrades(int line) {
 
         Boolean isDouble;
         double tempGrade = 0;
         for (int i = 0; i < line; i++) {
-            System.out.print("Enter the name of the Category " +(i+1)+ ": ");
+
+            double[] numArray = new double[2];
+
+            System.out.print("Enter the name of the Category " + (i + 1) + ": ");
             in.nextLine();
             String temp = in.nextLine();
             do {
@@ -35,8 +32,41 @@ public class Driver{
                     in.next();
                 }
             } while (!isDouble);
-            tempGrade = tempGrade / 100;
-            gradesMap.put(temp, tempGrade);
+
+            numArray[0] = tempGrade/100;
+            
+            gradesMap.put(temp, numArray);
+        }
+    }
+
+    private void inputWeight(){
+        System.out.println("Category\tPercentage");
+        for (Map.Entry<String, double[]> entry: gradesMap.entrySet()
+                ) {
+            double[] temp = entry.getValue();
+            System.out.println(entry.getKey()+"\t\t\t"+temp[0]*100+"%");
+        }
+        String temp;
+        for (Map.Entry<String, double[]> entry: gradesMap.entrySet()
+             ) {
+            boolean isInt;
+            double tempNum = 0;
+            do {
+                isInt = true;
+                System.out.print("Enter the weight of "+entry.getKey()+": ");
+                in.nextLine();
+                temp = in.nextLine();
+                try {
+                    tempNum = Double.parseDouble(temp);
+                } catch (Exception e){
+                    isInt = false;
+                    System.out.println("Weight must be an integer");
+                    //in.next();
+                }
+            } while (!isInt);
+            double[] tempArray = entry.getValue();
+            tempArray[1] = tempNum/100;
+            gradesMap.put(entry.getKey(), tempArray);
         }
     }
 
@@ -48,17 +78,38 @@ public class Driver{
             try {
                 System.out.print("Enter the number of weighted categories: ");
                 temp = in.nextInt();
+                if ((temp > 10) || (temp < 1)){
+                    throw new IntegerOutOfBoundsException("Number of categories must be between 1 and 10");
+                }
             } catch (InputMismatchException e){
                 isInt = false;
                 System.out.println("Number of categories must be an integer");
                 in.next();
+            } catch (IntegerOutOfBoundsException e){
+                System.out.println("Number of categories must be between 1 and 10");
+                isInt = false;
+                //in.next();
             }
         } while (!isInt);
         return temp;
     }
+    
+    private void display(){
+        System.out.println("Category\tPercentage\tWeight");
+        for (Map.Entry<String, double[]> entry: gradesMap.entrySet()
+             ) {
+            double[] temp = entry.getValue();
+            System.out.println(entry.getKey()+"\t\t\t"+temp[0]*100+"%"+"\t\t"+temp[1]);
+        }
+    }
+    
 
     public static void main(String[] args) {
         Driver driver = new Driver();
-        driver.getGrades(driver.getNumberOfCategories());
+        driver.inputGrades(driver.getNumberOfCategories());
+
+        driver.display();
+        driver.inputWeight();
+        driver.display();
     }
 }
---
+++ src/IntegerOutOfBoundsException.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Ken Sodetz on 12/14/2016.
+ */
+public class IntegerOutOfBoundsException extends Exception {
+    public IntegerOutOfBoundsException(String message){
+        super(message);
+    }
+}
