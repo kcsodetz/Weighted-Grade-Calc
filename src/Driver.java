f00b67cd1feb612829201c68ff05be6165e1607a
Ken Sodetz <kensodetz@gmail.com>
2016-12-16 15:53:59 -0600

    added other methods to make operations  simpler as well as comments to make operation more clear

--- src/Driver.java
+++ src/Driver.java
@@ -1,94 +1,109 @@
+import java.math.BigDecimal;
 import java.util.*;
+
+/**
+ * Created by Ken Sodetz on 12/14/2016.
+ */
 
 public class Driver{
 
     private HashMap <String, double[]> gradesMap;
     private Scanner in = new Scanner(System.in);
 
-
+    //constructor
     public Driver(){
         gradesMap = new HashMap<>();
     }
 
+    //method to get and check the initial percentages
     private void inputGrades(int line) {
 
         Boolean isDouble;
+        String tempName;
         double tempGrade = 0;
         for (int i = 0; i < line; i++) {
 
             double[] numArray = new double[2];
 
-            System.out.print("Enter the name of the Category " + (i + 1) + ": ");
-            in.nextLine();
-            String temp = in.nextLine();
+            System.out.println("Enter the name of the Category " + (i + 1) + ": ");
+            tempName = in.nextLine();
             do {
-                try {
-                    isDouble = true;
-                    System.out.print("Enter the percent of category " + temp + " (E.g: 94.5): ");
-                    tempGrade = in.nextDouble();
-                } catch (InputMismatchException e) {
-                    System.out.println("Percentage must be a Number");
+                isDouble = true;
+                System.out.print("Enter the percent of category " + tempName + " (E.g: 94.5): ");
+                tempGrade = checkDouble(in.nextLine());
+                if (tempGrade == 0)
                     isDouble = false;
-                    in.next();
-                }
             } while (!isDouble);
 
-            numArray[0] = tempGrade/100;
+            numArray[0] = tempGrade/100.0;
             
-            gradesMap.put(temp, numArray);
+            gradesMap.put(tempName, numArray);
         }
     }
 
+    //method to get and check the weights of the categories
     private void inputWeight(){
-        System.out.println("Category\tPercentage");
-        for (Map.Entry<String, double[]> entry: gradesMap.entrySet()
-                ) {
-            double[] tempArray = entry.getValue();
-            System.out.println(entry.getKey()+"\t\t\t"+tempArray[0]*100+"%");
-        }
+
+        //Displays Current Categories and Percentages
+        display();
+
+        //Holder temp variables
         String temp;
+        double tempNum = 0;
+
+        //Get the initial weights of the categories
         for (Map.Entry<String, double[]> entry: gradesMap.entrySet()
              ) {
             boolean isInt;
-            double tempNum = 0;
             do {
                 isInt = true;
                 System.out.print("Enter the weight of "+entry.getKey()+": ");
-                in.nextLine();
-                temp = in.nextLine();
-                try {
-                    tempNum = Double.parseDouble(temp);
-                } catch (Exception e){
+                tempNum = checkDouble(in.nextLine());
+                if (tempNum == 0)
                     isInt = false;
-                    System.out.println("Weight must be a number");
-                    //in.next();
-                }
             } while (!isInt);
+
+            //place weights into the Hashmap
             double[] tempArray = entry.getValue();
             tempArray[1] = tempNum/100;
             gradesMap.put(entry.getKey(), tempArray);
         }
-        double total = totalWeight();
-        do {
-            if (!(Math.abs(1 - total) < .00000001)) {
-                System.out.println("Weighted total must be equal to 1");
-                display();
-                do {
-                    System.out.print("Enter a category to edit: ");
-                    temp = in.nextLine();
-                    if (!gradesMap.containsKey(temp))
-                        System.out.println("The category does not exist");
-                    else {
-                        System.out.print("Enter the new Weight for " +temp+ ": ");
 
+        //checks to see if the total added up is equal to 1
+        boolean notValid, notDouble;
+        if (checkEquality(totalWeight())){
+            //do method
+        }
+        else {
+            do {
+                System.out.println("Total wights must be equal to 1");
+                do {
+                    notValid = false;
+                    display();
+                    System.out.print("Enter a category to edit its weight: ");
+                    temp = in.nextLine();
+                    if (!gradesMap.containsKey(temp)) {
+                        notValid = true;
+                        System.out.println("Category not found");
+                    } else {
+                        do {
+                            notDouble = false;
+                            System.out.print("Enter the new weight for " + temp + ": ");
+                            tempNum = checkDouble(in.nextLine());
+                            if (tempNum == 0)
+                                notDouble = true;
+                            double[] tempArray = gradesMap.get(temp);
+                            tempArray[1] = tempNum/100.0;
+                            gradesMap.put(temp, tempArray);
+                        } while (notDouble);
                     }
-                } while (!gradesMap.containsKey(temp));
-            }
-            total = totalWeight();
-        } while (!(Math.abs(1 - total) < .00000001));
+                } while (notValid);
+            } while (!checkEquality(totalWeight()));
+        }
 
     }
 
+    //adds up the total weights
     private double totalWeight(){
         double total = 0;
         for (Map.Entry<String, double[]> entry: gradesMap.entrySet()
@@ -99,18 +114,23 @@ public class Driver{
         return total;
     }
 
+    //parses and checks if the input is a valid double
     private double checkDouble(String input){
-
-
+        try {
+            double num = Double.parseDouble(input);
+            return num;
+        } catch (Exception e){
+            System.out.println(input + " is an invalid input. Must be a standard number.");
+        }
         return 0;
     }
 
-    private double checkDouble(double num){
-
-        return 0;
+    //checks to see if the given double is equal to 1 within a 3 decimal place of error
+    private boolean checkEquality(double num){
+        return (Math.abs(1 - num) < .001);
     }
 
-
+    //gets the number of categories for the user to input
     private int getNumberOfCategories(){
         int temp = 0;
         Boolean isInt;
@@ -129,22 +149,25 @@ public class Driver{
             } catch (IntegerOutOfBoundsException e){
                 System.out.println("Number of categories must be between 1 and 10");
                 isInt = false;
-                //in.next();
             }
         } while (!isInt);
         return temp;
     }
-    
+
+    //displays the category name, percent for the category, anc the weight of the category, for all categories given
     private void display(){
         System.out.println("Category\tPercentage\tWeight");
         for (Map.Entry<String, double[]> entry: gradesMap.entrySet()
              ) {
             double[] temp = entry.getValue();
-            System.out.println(entry.getKey()+"\t\t\t"+temp[0]*100+"%"+"\t\t"+temp[1]);
+            String tempStr = Double.toString(temp[0]*100.0);
+            BigDecimal bigDecimal = new BigDecimal((tempStr));
+            BigDecimal bd = bigDecimal.setScale(2, BigDecimal.ROUND_HALF_UP);
+            System.out.println(entry.getKey()+"\t\t\t"+bd.toString()+"%"+"\t\t"+temp[1]);
         }
     }
-    
 
+    //main method
     public static void main(String[] args) {
         Driver driver = new Driver();
         driver.inputGrades(driver.getNumberOfCategories());
